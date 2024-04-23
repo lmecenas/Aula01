358c9d8f3b4e092cbd33f0a58643d127ddab45ca
Luiz Mecenas <lsmecenas@gmail.com>
2024-04-23 19:48:58 -0300

    adicionar o desafio gitpush

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+nome_cliente = input("Digite o seu nome: ")
+salario = float(input("Digite o seu salario: "))
+bonus = float(input("Digite o valor do bonus: "))
+constante_bonus = 1000
+kpi = constante_bonus + salario*bonus
+
+print(f"Parabéns {nome_cliente}, seu bônus foi de: {kpi}")
